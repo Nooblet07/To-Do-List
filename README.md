f270f1547e242e4ccb10b08a15d4e0c2bbef89ca
Nooblet07 <carlow2010@gmail.com>
2023-05-23 14:56:23 -0400

    Updated html, js, css, and README. Essentially works.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # To-Do-List
+
+<!-- Have a daily to-do-list for myself -->
+<!-- When you complete a task, all you need to do is double click the item and is will line through as completed->
+<!-- Created a form at the bottom of the page to add last minute task items and appended below the form box. Attempted to have it append above the h2 header, but was unsuccessful.-->
---
+++ index.js
@@ -0,0 +1,28 @@
+// Click on a subtask will put a line through indicating it is complete.
+function toggleLineOut(element) {
+    element.classList.toggle('line-out');
+  }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('#addItemForm');
+    const itemInput = document.querySelector('#itemInput');
+    const myList = document.querySelector('#myList');
+    const list = document.querySelector('#list');
+    // const wrapper = document.getElementById('wrapper'); // Added to append above h2
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+
+        const newItem = document.createElement('li');
+        newItem.textContent = itemInput.value;
+        newItem.ondblclick = function() {
+            toggleLineOut(this);
+        };
+
+        list.appendChild(newItem); // Append the new item to the list
+        // list.insertBefore(newItem, wrapper.nextSibling); //Append above h2
+
+        itemInput.value = ''; // Clear the input field
+        itemInput.focus(); // Set focus back to the input field
+    });
+});
