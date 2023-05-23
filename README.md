a2f75922d1efd0532daf2d8885fcc7d9652bceaf
Nooblet07 <carlow2010@gmail.com>
2023-05-23 17:23:50 -0400

    Fine tuned some things in my html, js, and css.

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 <!-- Have a daily to-do-list for myself -->
 <!-- When you complete a task, all you need to do is double click the item and is will line through as completed->
-<!-- Created a form at the bottom of the page to add last minute task items and appended below the form box. Attempted to have it append above the h2 header, but was unsuccessful.-->
+<!-- Created a form at the bottom of the page to add last minute task item and appended below the form box. Can double click to line through once completed as well. Attempted to have it append above the h2 header, but was unsuccessful.-->
+<!-- Made a timestamp option when adding new items to the list -->
+
+<!-- I may have mis-interpreted what was required. I didn't think it was a to-do list created by the user with the user adding items.  I just thought to create a daily list with the ability to add additional items. By the time I thought to modify everything, it was 3PM CST and decided to stay with my original idea. -->
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Click on a subtask will put a line through indicating it is complete.
+// Double click on a subtask will put a line through indicating it is complete.
 function toggleLineOut(element) {
     element.classList.toggle('line-out');
   }
@@ -15,6 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const newItem = document.createElement('li');
         newItem.textContent = itemInput.value;
+        
+        const timestamp = new Date().toLocaleString(); // Get the current timestamp
+        const timestampSpan = document.createElement('span');
+        timestampSpan.textContent = timestamp;
+        timestampSpan.textContent = ' added on: ' + timestamp;
+        timestampSpan.className = 'timestamp';
+        newItem.appendChild(timestampSpan);
         newItem.ondblclick = function() {
             toggleLineOut(this);
         };
@@ -23,6 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // list.insertBefore(newItem, wrapper.nextSibling); //Append above h2
 
         itemInput.value = ''; // Clear the input field
-        itemInput.focus(); // Set focus back to the input field
+        
     });
 });
